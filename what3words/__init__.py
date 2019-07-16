43086fbfaf2cebb217a1d124c79f987f327392f3
Simon Barnett <simon@what3words.com>
2019-07-16 17:20:43 +0100

    Update version number handling

--- what3words/__init__.py
+++ what3words/__init__.py
@@ -6,11 +6,11 @@ from .what3words import Circle
 from .what3words import Coordinates
 from .what3words import BoundingBox
 
-import version
+from .version import __version__ as v
 
 __title__ = 'what3words'
 __author__ = 'what3words'
 __author_email__ = 'development@what3words.com'
-__version__ = version.__version__
+__version__ = v
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2017 what3words'
