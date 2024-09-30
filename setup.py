fe95f0fff85f8350e88b145c8ff2c01f7e02268c
Manuela Sabatino <mn.sabatino@gmail.com>
2024-09-30 16:20:43 +0100

    add the version and readme to the next pypi package

--- setup.py
+++ setup.py
@@ -1,18 +1,38 @@
 from codecs import open
+"""
+This script sets up the what3words Python API wrapper library.
+
+It reads the version from the version.py file, the requirements from the requirements.txt file,
+and the long description from the README.md file. It then uses setuptools to package the library.
+
+Attributes:
+    version (dict): A dictionary to store the version information read from version.py.
+    requires (list): A list of dependencies read from requirements.txt.
+    long_description (str): The content of the README.md file used as the long description.
+
+Functions:
+    setup(): Configures the package using setuptools.
+"""
 import re
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+import os
 
 # Read the version from version.py
 version = {}
-with open("version.py") as fp:
+version_file_path = os.path.join(os.path.dirname(__file__), "what3words", "version.py")
+with open(version_file_path) as fp:
     exec(fp.read(), version)
 
 with open("requirements.txt") as f:
     requires = f.read().splitlines()
+
+# Read the contents of the README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="what3words",
@@ -20,7 +40,9 @@ setup(
     author="what3words",
     author_email="support@what3words.com",
     url="https://github.com/what3words/w3w-python-wrapper",
-    description="What3words API wrapper library",
+    description="what3words Python API wrapper library",
+    long_description=long_description, 
+    long_description_content_type="text/markdown",
     license="MIT",
     packages=["what3words"],
     package_dir={"what3words": "what3words"},
